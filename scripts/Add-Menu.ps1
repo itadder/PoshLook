8f6c3e0848956dad0b9036737fc087b8fe0f33e3
Nicholas Hansen <hoiois1996@gmail.com>
2016-01-23 23:20:42 +0100

    Comments, fixes and tiny additions
    I fixed module Import, I commented Add-Menu and Add-MenuItem, and I
    added a feature to set the terminal backgroundcolor based on the menu.

--- scripts/Add-Menu.ps1
+++ scripts/Add-Menu.ps1
@@ -1,14 +1,38 @@
+<#
+.Synopsis
+   Imports a JSON file that contains the layout for the menu
+.DESCRIPTION
+   Imports a JSON file that contains the layout for the Menu and calls Add-MenuItem for each object within the json file under "Objects".
+   Look at example.json for formating of JSON files.
+   It returns the imported JSON file so it can be processed by things like menu navigation and submenus.
+.EXAMPLE
+   Add-MenuItem -filepath example.json
+   Draws a menu based on example.json
+.EXAMPLE
+   Add-Menu -filepath C:\example\customlayout\custommenu.json
+   Draws a menu based on C:\example\customlayouy\custommenu.json
+.LINK
+   Github project: https://github.com/poshlook/PoshLook
+#>
 Function Add-Menu{
     param(
+        #Filepath to the JSON File.
         [string]$filepath
     )
-    
-    cls
+
+    #Variables
+    $DefaultMenuColor = "DarkMagenta" #Our menus default background color
+    #End Variables
+
     Push-Location
     cd $PSScriptRoot
-    [string]$json = Get-Content "MainMenu.design" -Raw
+    [string]$json = Get-Content "$filepath" -Raw
     $InputFile = ConvertFrom-Json -InputObject $json
     Pop-Location
+
+    if ($InputFile.Menu[0].Backgroundcolor -ne ""){$host.UI.RawUI.BackgroundColor = $InputFile.Menu[0].Backgroundcolor}
+    else {$host.UI.RawUI.BackgroundColor = "DarkMagenta"}
+    cls
 
     foreach ($i in 0..($InputFile.Objects.Count-1)){
         $HashArguments = @{text = $InputFile.Objects[$i-1].Text; x = $InputFile.Objects[$i-1].x; y = $InputFile.Objects[$i-1].y}
--- scripts/Add-MenuItem.ps1
+++ scripts/Add-MenuItem.ps1
@@ -1,9 +1,32 @@
+<#
+.Synopsis
+   Adds text, optionaly with color to the x and y positions of the interface.
+.DESCRIPTION
+   Adds text, optionaly with color to the x and y positions of the interface.
+.EXAMPLE
+   Add-MenuItem -text "You have new mail." -x 0 -y 10 -backgroundcolor "Blue"
+   Adds "You have new mail." a x-coordinate 0 and y coordinate 10 with the backgroundcolor of the letters being blue.
+.EXAMPLE
+   Add-MenuItem -text "[Contacts]" -x 6 -y 20 -textcolor "green"
+   Adds "[Contacts]" a x-coordinate 6 and y coordinate 20 with the text color of the letters being green.
+.LINK
+   Github project: https://github.com/poshlook/PoshLook
+#>
 function Add-MenuItem{
     param(
+        #Input text that will be displayed
         [string]$text,
+
+         #x coordinate
         [int]$x,
+
+         #y coordinate
         [int]$y,
+
+        #Backgroundcolor (Of the text, not the screen)
         [string]$backgroundcolor="DarkMagenta",
+
+        #Text color
         [string]$textcolor="DarkYellow"
     )
     $position=$host.ui.rawui.cursorposition
