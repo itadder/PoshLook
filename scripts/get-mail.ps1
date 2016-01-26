a883058e205957ea9f9aed3dfa787c2241f5c390
beaudryj <beaudryj@wit.edu>
2016-01-25 21:43:12 -0500

    started working with imapx

--- scripts/get-mail.ps1
+++ scripts/get-mail.ps1
@@ -1,6 +1,26 @@
+<#                                                                                                              
+.Synopsis                                                                                                       
+.DESCRIPTION                                                                                                    
+.EXAMPLE                                                                                                        
+   Get-Mail -provider "imap.google.com"                                                  
+.LINK                                                                                                           
+   Github project: https://github.com/poshlook/PoshLook                                                         
+#>
+   
 function Get-Mail(
 [Parameter]
-[string]$test
+#mail provider to connect to
+[string]$provider
+[string]$auth
 )
 {
+Add-Type -Path "$psScriptRoot\dlls\ImapX.dll"
+
+$client = New-Object Imapx.ImapClient("$provider", 993,$true)
+$credentials = New-Object ImapX.Authentication.
+
+$client.connect()
+$client.folders.
 }
+
+
