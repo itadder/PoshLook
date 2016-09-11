ca964ab2aed14e36be26122586795b95831ccf32
Nicholas Hansen <hoiois1996@gmail.com>
2016-09-11 16:06:22 +0200

    Made changes to Add-Menu that allowes for automatic sorting of otherwise overlapping objects.

--- scripts/Add-Menu.ps1
+++ scripts/Add-Menu.ps1
@@ -30,7 +30,7 @@ Function Add-Menu{
     $IncorrectJSON = "relativity in JSON is incorrect, must be true or false."
     #End Variables
 
-    Push-Location
+    Push-Location #This part gets the json files and imports it
     cd $PSScriptRoot
     [string]$json = Get-Content "$filepath" -Raw
     $InputFile = ConvertFrom-Json -InputObject $json
@@ -43,18 +43,23 @@ Function Add-Menu{
     $ch = [console]::WindowHeight
     $cw = [console]::WindowWidth
 
+    $CollectionHashArguments = @{} #Because there could be overlaps due to relative positioning, I add all the items to a hashtable first...
+    #Then I check the coordinates, and increment duplicates until everything is unique
     foreach ($i in 0..($InputFile.Objects.Count-1)){
         $HashArguments = @{}
         
-        if ($InputFile.Objects[$i-1].yisrelative = "true"){
+        #region Y allignment
+        if ($InputFile.Objects[$i-1].yisrelative = "true"){ #Sets the Y coordinates. Can be relative or fixed. Warning: Relative menu items can overlap eachother
             $HashArguments.y = $ch/100*$InputFile.Objects[$i-1].y
         } elseif ($InputFile.Objects[$i-1].yisrelative = "false") {
             $HashArguments.y = $InputFile.Objects[$i-1].y
         } else{
             throw $IncorrectJSON
         }
+        #endregion
         
-        if ($InputFile.Objects[$i-1].Alignment -eq "left"){
+        #region X allignment
+        if ($InputFile.Objects[$i-1].XAlignment -eq "left"){
             if ($InputFile.Objects[$i-1].xisrelative = "true"){
                 $HashArguments.x = $cw/100*$InputFile.Objects[$i-1].x
             } elseif ($InputFile.Objects[$i-1].xisrelative = "false") {
@@ -62,7 +67,7 @@ Function Add-Menu{
             } else{
                 throw $IncorrectJSON
             }
-        } elseif ($InputFile.Objects[$i-1].Alignment -eq "right"){
+        } elseif ($InputFile.Objects[$i-1].XAlignment -eq "right"){
             if ($InputFile.Objects[$i-1].xisrelative = "true"){
                 if ($InputFile.Objects[$i-1].IsVariable){
                     $HashArguments.x = ($cw/100*$InputFile.Objects[$i-1].x)-($HashTable.($InputFile.Objects[$i-1].Text)).Length
@@ -79,7 +84,7 @@ Function Add-Menu{
                 throw $IncorrectJSON
             }
         }
-        elseif ($InputFile.Objects[$i-1].Alignment -eq "center"){
+        elseif ($InputFile.Objects[$i-1].XAlignment -eq "center"){
             if ($InputFile.Objects[$i-1].xisrelative = "true"){
                 if ($InputFile.Objects[$i-1].IsVariable){
                     $HashArguments.x = ($cw/100*$InputFile.Objects[$i-1].x)-(($HashTable.($InputFile.Objects[$i-1].Text)).Length/2)
@@ -96,7 +101,7 @@ Function Add-Menu{
                 throw $IncorrectJSON
             }
         } else {
-            throw "Alignment in JSON is incorrect: must be left, right or center"
+            throw "X Alignment in JSON is incorrect: must be left, right or center"
         }
         if ($InputFile.Objects[$i-1].IsVariable){
             $HashArguments.text = $HashTable.($InputFile.Objects[$i-1].Text)
@@ -105,7 +110,32 @@ Function Add-Menu{
         }
         if ($InputFile.Objects[$i-1].Backgroundcolor){ $HashArguments.Backgroundcolor = $InputFile.Objects[$i-1].Backgroundcolor }
         if ($InputFile.Objects[$i-1].Textcolor){ $HashArguments.Textcolor = $InputFile.Objects[$i-1].Textcolor }
-        Add-MenuItem @HashArguments
+
+        #Here I'm rounding to the nearest number. I'm doing it down here to keep the code above a little less cluttered than it already is.
+        $HashArguments.x = [System.Math]::Round($HashArguments.x)
+        $HashArguments.y = [System.Math]::Round($HashArguments.y)
+
+        $CollectionHashArguments.Add($InputFile.Objects[$i-1].Index, $HashArguments)
+        #Add-MenuItem @HashArguments
     }
+
+    #This may take a little explaining. What I'm doing here is reading out every effective x Value and writing it to an array.
+    #If the number already exists, it increments it in the hashtable and then adds it to the array.
+    #This prevents menu items being overridden by overlapping items
+    #$CollectionHashArguments = ($CollectionHashArguments.GetEnumerator() | Sort-Object Name)
+    $CollectionHashArguments.GetType() | Out-File -FilePath D:\temp\test.txt -Append
+    $CollectionHashArguments | Out-File -FilePath D:\temp\test.txt -Append
+    $yCoordinates = @()
+    foreach ($i in ($CollectionHashArguments.Keys | Sort-Object)) {
+        while ( $yCoordinates.Contains($CollectionHashArguments.Item($i).item("y")) ) {
+            $CollectionHashArguments.item($i).item("y") += 1
+        }
+        $yCoordinates += $CollectionHashArguments.Item($i).item("y")
+        $PassValue = @{} #I could simplify the next 4 lines but I want to make it easily understandable
+        $PassValue = $CollectionHashArguments.Item($i)
+        $PassValue | Out-File -FilePath D:\temp\test.txt -Append
+        Add-MenuItem @PassValue
+    }
+    $yCoordinates | Out-File -FilePath D:\temp\test.txt -Append
     return $InputFile
 }
