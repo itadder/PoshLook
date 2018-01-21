3a3df5b9787232d3ffea586708898f0c86133c3e
Chirishman <Chirishman@users.noreply.github.com>
2018-01-21 16:07:43 -0500

    Prototype TUI window (unsure why 'close' is throwing an error)

---
+++ scripts/PrototypeTUIWindow.ps1
@@ -0,0 +1,35 @@
+﻿function Show-TUIWindow {
+    Param()
+    gci "$PSScriptRoot\dll\TUI\" | select -ExpandProperty FullName | Import-Module -Force
+    
+    <#
+    $a = [tuibase.window]::new()
+    $a.Size = [tuibase.console.coordinates]::new(60,30)
+    $a.Location = [tuibase.console.coordinates]::new(10,3)
+    $a.Text = 'Hello World'
+    $b = [tuibase.textpanel]::new()
+    $b.Text = "Hello World 1"
+    $b.Location = [tuibase.console.Coordinates]::New(1, 1)
+    $b.Size = [tuibase.console.Coordinates]::New(57, 5)
+    $a.AddControl($b)
+
+    #>
+
+    $MainMenu = [TuiBase.PopUpMenu]::new(
+        'Main Menu',
+        (
+            [TuiBase.PopUpMenuItem[]]@(
+                [TuiBase.PopUpMenuItem]::new("Controls",{gci | Out-GridView}),
+                [TuiBase.PopUpMenuItem]::new("TextPanel",{Write-host 'Hello'}),
+                [TuiBase.PopUpMenuItem]::new("Windows and Dialogs",{Write-host 'World'}),
+                [TuiBase.PopUpMenuItem]::new("Exit",{$MainMenu.Close()})
+            )
+        )
+    )
+
+
+
+    $WindowRuntime = [TuiBase.WindowRuntime]
+    $WindowRuntime::Initialize()
+    $WindowRuntime::Run([Tui.TextConsole.ConsoleImpl]::new(), $MainMenu)
+}
