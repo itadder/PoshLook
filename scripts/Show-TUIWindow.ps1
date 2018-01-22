080641a6eb8c63b193c88d45607c523678028a12
Chirishman <Chirishman@users.noreply.github.com>
2018-01-21 23:08:44 -0500

    Removing Non-TUI relevat files

--- scripts/Show-TUIWindow.ps1
+++ scripts/Show-TUIWindow.ps1
@@ -1,19 +1,6 @@
 ﻿function Show-TUIWindow {
     Param()
     gci "$PSScriptRoot\dll\TUI\" | select -ExpandProperty FullName | Import-Module -Force
-    
-    <#
-    $a = [tuibase.window]::new()
-    $a.Size = [tuibase.console.coordinates]::new(60,30)
-    $a.Location = [tuibase.console.coordinates]::new(10,3)
-    $a.Text = 'Hello World'
-    $b = [tuibase.textpanel]::new()
-    $b.Text = "Hello World 1"
-    $b.Location = [tuibase.console.Coordinates]::New(1, 1)
-    $b.Size = [tuibase.console.Coordinates]::New(57, 5)
-    $a.AddControl($b)
-
-    #>
 
     $MainMenu = [TuiBase.PopUpMenu]::new(
         'Main Menu',
